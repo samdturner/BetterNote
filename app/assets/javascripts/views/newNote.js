c246f1f6babd6f6686ac3d931e778a3215788bfa
Your Actual Name <sam3turner@gmail.com>
2015-06-01 16:17:49 -0700

    Added drop down menu for paragraph alignment

--- app/assets/javascripts/views/newNote.js
+++ app/assets/javascripts/views/newNote.js
@@ -6,7 +6,29 @@ BetterNote.Views.NewNote = Backbone.View.extend({
   template: JST['new_note'],
 
   render: function () {
-    var content = this.template();
+    var colorArr = [
+                        'rgb(153, 51, 0)', 'rgb(153, 51, 0)',
+                        'rgb(51, 51, 0)', 'rgb(0, 51, 0)',
+                        'rgb(0, 51, 102)', 'rgb(0, 0, 128)',
+                        'rgb(51, 51, 153)', 'rgb(51, 51, 51)',
+                        'rgb(128, 0, 0)', 'rgb(255, 102, 0)',
+                        'rgb(128, 128, 0)', 'rgb(0, 128, 0)',
+                        'rgb(0, 128, 128)', 'rgb(0, 0, 255)',
+                        'rgb(102, 102, 153)', 'rgb(128, 128, 128)',
+                        'rgb(255, 0, 0)', 'rgb(255, 102, 0)',
+                        'rgb(153, 204, 0)', 'rgb(51, 153, 102)',
+                        'rgb(51, 204, 204)', 'rgb(51, 204, 204)',
+                        'rgb(51, 102, 255)', 'rgb(128, 0, 128)',
+                        'rgb(153, 153, 153)', 'rgb(255, 0, 255)',
+                        'rgb(255, 204, 0)', 'rgb(255, 255, 0)',
+                        'rgb(0, 204, 255)', 'rgb(153, 51, 102)',
+                        'rgb(192, 192, 192)', 'rgb(255, 153, 204)',
+                        'rgb(255, 204, 153)', 'rgb(255, 255, 153)',
+                        'rgb(204, 255, 204)', 'rgb(204, 255, 255)',
+                        'rgb(153, 204, 255)', 'rgb(204, 153, 255)',
+                        'rgb(255, 255, 255)', 'rgb(0, 80, 255)' 
+                     ]
+    var content = this.template({colorArr: colorArr});
     this.$el.html(content);
     return this;
   }
